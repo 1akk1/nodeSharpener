4f73517f35f44fb7fe4ad354d99e43f2a328ac3b
Akash Raghav <94389454+1akk1@users.noreply.github.com>
2024-03-31 22:54:45 +0530

    fatArrow,studentObject

--- firstApp.js
+++ firstApp.js
@@ -1,1 +1,15 @@
 console.log("hello World")
+
+
+
+const output = (a,b) => {return a*b};
+console.log(output(4,4))
+
+
+const student = {
+    name: "john doe",
+    age: 19,
+    gender: "male"
+}
+
+console.table(student)
