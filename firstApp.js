d2b52cd5814606d10f40d049f8564fdf0b7b2423
Akash Raghav <94389454+1akk1@users.noreply.github.com>
2024-03-31 23:41:07 +0530

    arrayMapping, spread_and_rest operators

--- firstApp.js
+++ firstApp.js
@@ -1,15 +1,28 @@
-console.log("hello World")
+const arr = ['apple','oranges','','mango','','lemon']
+
+
+const output  = arr.map((element)=>{
+    if(element === ''){
+        return('empty string')
+        }else{
+            return element
+        }
+})
+
+console.table(output)
+
+
+/* 3.  const hobbies does not change because it is stored in th same memory location without the const properties,
+we are now changing the const but the thing const is pointing towards. */ 
+
+/*Spread operaror (...) is used to spread out the array or object to be assigned to another variable(used to pull)*/
+
+/* 4. Spread operator creates a new objet or array*/
+
+/* 5. Rest operator(...) is different from spread because it is used to merge rather than pulling an array or object(used to merge) */
 
 
 
-const output = (a,b) => {return a*b};
-console.log(output(4,4))
 
 
-const student = {
-    name: "john doe",
-    age: 19,
-    gender: "male"
-}
 
-console.table(student)
