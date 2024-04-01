aa8b64cf9f5238cccd187554025fff5850e4282c
Akash Raghav <94389454+1akk1@users.noreply.github.com>
2024-04-01 22:36:06 +0530

    Created HTML page writing data and reading from a file

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 const http  = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
-    console.log(res)
     const url = req.url
     const method = req.method
-    if(url === '/home'){
+    if(url === '/') {
+        const messageContent = fs.readFileSync('message.txt');
         res.write('<html>')
-        res.write('<h2>Welcome Home</h2>')
-        res.write('</html>')
-
-        return res.end
-
-    }else if(url === 'about'){
-        res.write('<html>')
-        res.write('<h2>Welcome to about us page</h2>')
-        res.write('</html>')
-        return res.end
-
-    }else if(url === '/node'){
-        res.write('<html>')
-        res.write('<h2>Welcome to my node js project</h2>')
-        res.write('</html>')
+        res.write(`<h3>${messageContent}</h3>`);
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action = "/message" method ="POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>' )
+        res.write('</html>')            
         return res.end
     }
+    
+    if(url ===  '/message'  && method === 'POST') { 
+        const body = []
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        })    
+        req.on('end' ,()=>{
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end();
+    }
 })
-
-
-
-
 server.listen(3000)
