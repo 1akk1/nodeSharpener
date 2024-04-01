26339ec73aa8d5394156fc5f15d61726dc11a4db
Akash Raghav <94389454+1akk1@users.noreply.github.com>
2024-04-01 11:13:15 +0530

    destructuring_spread_rest example

--- firstApp.js
+++ firstApp.js
@@ -1,15 +1,15 @@
-const arr = ['apple','oranges','','mango','','lemon']
+// const arr = ['apple','oranges','','mango','','lemon']
 
 
-const output  = arr.map((element)=>{
-    if(element === ''){
-        return('empty string')
-        }else{
-            return element
-        }
-})
+// const output  = arr.map((element)=>{
+//     if(element === ''){
+//         return('empty string')
+//         }else{
+//             return element
+//         }
+// })
 
-console.table(output)
+// console.table(output)
 
 
 /* 3.  const hobbies does not change because it is stored in th same memory location without the const properties,
@@ -17,9 +17,39 @@ we are now changing the const but the thing const is pointing towards. */
 
 /*Spread operaror (...) is used to spread out the array or object to be assigned to another variable(used to pull)*/
 
-/* 4. Spread operator creates a new objet or array*/
+/* 4. Spread operator creates a new object or array*/
 
 /* 5. Rest operator(...) is different from spread because it is used to merge rather than pulling an array or object(used to merge) */
+
+
+
+
+
+/* Spread Oprerator
+const obj1 = {
+    name: 'akash',
+    age: 20
+}
+
+const obj2 = {...obj1}
+console.log(obj2)*/
+
+/* Rest Operator
+const obj3 = (...obj)=>{return obj}
+console.log(obj3(1,2,3)) */
+
+
+/*   Destructuring
+    const arr = ['ak','rg'];
+    const [name , lastname] = arr;
+    console.log(name,lastname) //in array
+
+    const obj = {name : 'ak', age: 23}
+    const { name , age } = obj;
+    console.log(name,age) //in object
+*/
+
+
 
 
 
